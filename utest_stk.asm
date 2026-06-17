b231c33d354dcbd0d22448011fed9af2dd0356b6
William <willdev2025@outlook.com>
2026-06-16 20:31:58 -0700

    kinda broke userspace for now, gonna work on that later im sooooo tiredddddd from deuging all day

--- utest_stk.asm
+++ utest_stk.asm
@@ -3,15 +3,18 @@ global _start
 
 section .text
 _start:
+    sub esp, 8
+    mov dword [esp], 0x6C6C6548
+    mov dword [esp+4], 0x000A216F
+
     mov eax, 3
     mov ebx, 1
-    lea ecx, [rel msg]
+    mov ecx, esp
     mov edx, 7
     int 0x80
+
+    add esp, 8
 
     mov eax, 1
     mov ebx, 0
     int 0x80
-
-section .rodata
-msg: db "Hello!",10
