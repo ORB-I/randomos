c42543bfcf9629e0182c2f4262e235a0c100012c
ORB-I <nerd2testly79@gmail.com>
2026-09-03 18:00:17 -0600

    added randomos system test

---
+++ user/progs/rosyst.c
@@ -0,0 +1,50 @@
+#include <io.h>
+#include <kbd.h>
+#include <sys/process.h>
+#include <sys/sysfn.h>
+
+#define BAR_WIDTH 28
+#define CORE_ROWS 4
+
+static void print_bar(const char* label, const char* value) {
+    printf("%-12s %3s [", label, value);
+    for (int i = 0; i < BAR_WIDTH; i++) {
+        putchar('=');
+    }
+    printf("]\n");
+}
+
+static void draw_dashboard(void) {
+    termctl(TCTL_CLEAR, 0);
+    printf("RandomOS system monitor\n\n");
+
+    for (int core = 0; core < CORE_ROWS; core++) {
+        char label[16];
+        snprintf(label, sizeof(label), "Core %d", core);
+        print_bar(label, "N/A");
+    }
+    print_bar("Ram Amount", "N/A");
+    print_bar("Swap Amount", "N/A");
+
+    printf("\n%-24s | %-10s | %s\n", "Name", "CPU Usage", "PID");
+    printf("%-24s | %-10s | %d\n", "rosyst", "N/A", getpid());
+    printf("\nPress Ctrl+C to exit.\n");
+}
+
+int main(void) {
+    int ctrl_pressed = 0;
+    draw_dashboard();
+
+    for (;;) {
+        u8 scancode = kbd_get_raw_to(100);
+        if (scancode == 0) continue;
+
+        if (scancode == 0x1d) {
+            ctrl_pressed = 1;
+        } else if (scancode == 0x9d) {
+            ctrl_pressed = 0;
+        } else if (ctrl_pressed && (scancode == 0x2e || scancode == 0xae)) {
+            return 0;
+        }
+    }
+}
